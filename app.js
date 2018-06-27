925016fe17b1ecd7a2af58ec694a8a7bf646a029
teodoratanasov <t.atanasov1516@gmail.com>
2018-06-27 19:04:27 +0300

    Back end done

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+app.use(bodyParser.json());
+
 Genre = require('./models/genre');
 Game = require('./models/game');
 //connect to mongodb
@@ -22,6 +24,39 @@ app.get('/api/genres', function(req, res){
     });
 });
 
+app.post('/api/genres', function(req, res){
+    var genre = req.body;
+    Genre.addGenre(genre, function(err, genre){
+        if(err){
+            throw err;
+        }
+        res.json(genre);
+    });
+});
+
+app.put('/api/genres/:_id', function(req, res){
+    var id = req.params._id;
+    var genre = req.body;
+    Genre.updateGenre(id, genre, {}, function(err, genre){
+        if(err){
+            throw err;
+        }
+        res.json(genre);
+    });
+});
+
+app.delete('/api/genres/:_id', function(req, res){
+    var id = req.params._id;
+    var genre = req.body;
+    Genre.removeGenre(id, genre, {}, function(err, genre){
+        if(err){
+            throw err;
+        }
+        res.json(genre);
+    });
+});
+
+
 app.get('/api/games', function(req, res){
     Game.getGames(function(err, games){
         if(err){
@@ -39,6 +74,39 @@ app.get('/api/games/:_id', function(req, res){
         res.json(game);
     });
 });
+
+app.post('/api/games', function(req, res){
+    var game = req.body;
+    Game.addGame(game, function(err, game){
+        if(err){
+            throw err;
+        }
+        res.json(game);
+    });
+});
+
+app.put('/api/games/:_id', function(req, res){
+    var id = req.params._id;
+    var game = req.body;
+    Game.updateGame(id, game, {}, function(err, game){
+        if(err){
+            throw err;
+        }
+        res.json(game);
+    });
+});
+
+app.delete('/api/games/:_id', function(req, res){
+    var id = req.params._id;
+    var game = req.body;
+    Game.removeGame(id, game, {}, function(err, game){
+        if(err){
+            throw err;
+        }
+        res.json(game);
+    });
+});
+
 
 app.listen(3000);
 console.log('Running on port 3000...');
--- models/game.js
+++ models/game.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 // Genre Schema
 var gameSchema = mongoose.Schema({
-    name:{
+    title:{
         type: String,
         required: true
     },
@@ -39,4 +39,29 @@ module.exports.getGames = function(callback, limit){
 // Get Game ID
 module.exports.getGameById = function(id, callback, limit){
     Game.findById(id, callback);
+}
+
+// Add Game
+module.exports.addGame = function(game, callback){
+    Game.create(game,callback);
+}
+
+// Update Game
+module.exports.updateGame = function(id, game, options, callback){
+    var query = {_id: id};
+    var update = {
+        title: game.title,
+        genre: game.genre,
+        description: game.description,
+        creator: game.creator,
+        image_url: game.image_url,
+        download_url: game.download_url
+    } 
+    Game.findOneAndUpdate(query, update, options, callback);
+}
+
+// Delete Game
+module.exports.removeGame = function(id, callback){
+    var query = {_id: id};
+    Game.remove(query,callback);
 }
