b68110040dc650350947eb2c8872b5996ec2180b
teodoratanasov <t.atanasov1516@gmail.com>
2018-06-28 20:00:02 +0300

    front end almost done

--- public/controllers/games.js
+++ public/controllers/games.js
@@ -4,8 +4,15 @@ myApp.controller('GamesController', ['$scope', '$http', '$location', '$routePara
     console.log('GamesController ready...');
 
     $scope.getGames = function(){
-        $http.get('/api/games').then(function(response){
-            $scope.games = response;
+        $http.get("/api/games").then(function(response){
+            $scope.games = response.data;
+        });
+    }
+
+    $scope.getGame = function(){
+        var id = $routeParams.id;
+        $http.get("/api/games/"+id).then(function(response){
+            $scope.game = response.data;
         });
     }
 }]);
